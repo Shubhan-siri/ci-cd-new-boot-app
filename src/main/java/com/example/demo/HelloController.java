0169bc0e751a83d9380d923c5e814e9577390bba
Shubhan-siri <shubhanshubusines786@gmail.com>
2025-08-25 04:14:16 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
   @GetMapping("/")
   public String home() {
-    return "Hello from Jenkins CI/CD! 🎉";
+    return "Hello from Shubhanshu Jenkins! 🎉";
   }
 }
